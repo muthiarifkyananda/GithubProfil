e7aec6d07f6a86506ee6c06b33e6a9976cc5b1ca
muthiarifkyananda <muthiarifkyananda@gmail.com>
2024-12-07 21:18:55 +0700

    push.

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+val apikeyPropertiesFile = rootProject.file("apikey.properties")
+val apikeyProperties = Properties()
+
+if (apikeyPropertiesFile.exists()) {
+    apikeyProperties.load(FileInputStream(apikeyPropertiesFile))
+} else {
+    throw GradleException("File apikey.properties tidak ditemukan di root project.")
+}
+
+android {
+    buildFeatures {
+        buildConfig = true // Mengaktifkan BuildConfig
+    }
+    namespace = "id.ac.polbeng.muthiaraa.githubprofile"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "id.ac.polbeng.muthiaraa.githubprofile"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        buildConfigField ("String", "ACCESS_TOKEN", "\"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\"")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("com.airbnb.android:lottie:5.2.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    kapt("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.9")
+}
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}
